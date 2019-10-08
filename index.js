6a25008f92c0dacd285fbff37fbc7c0c67c04817
serhiiShovkovyi <sergshovkanser@gmail.com>
2019-10-08 21:20:14 +0300

    third commit

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ const buttton = document.getElementById("add"),
   descInp = document.querySelector(".desc"),
   priorityInp = document.querySelector(".priority"),
   dateTime = document.querySelector(".date");
+
+const editName = document.querySelector(".name-edit"),
+  editDesc = document.querySelector(".desc-edit"),
+  editPriority = document.querySelector(".priority-edit"),
+  editDate = document.querySelector(".date-edit"),
+  exitBtn = document.querySelector(".exit"),
+  btnEdit = document.querySelector(".btn-edit");
+
 const div = document.createElement("div");
 div.className += "style-of-text obj bordered";
 const br = document.createElement("br");
@@ -39,13 +47,29 @@ window.onload = () => {
   renderingAllListItem(res);
 };
 
+const editItem = event => {
+  const id = event.target.getAttribute("key");
 
+  const item = JSON.parse(localStorage.getItem("0"))[id];
+  const { name, description, priority, date, key } = item;
+  editName.value = name;
+  editDesc.value = description;
+  editPriority.value = priority;
+  editDate.value = date;
+  const error = document.querySelector(".error");
+  error.className += " d-none";
+  error.classList.remove("d-block");
+  const editDialog = document.querySelector(".edit-dialog");
+  editDialog.className += " d-block";
+  editDialog.classList.remove("d-none");
+  btnEdit.setAttribute("key", key);
+};
 
 const deleteItem = event => {
   const id = event.target.getAttribute("key");
   console.log(id);
 
-  if (state.listOfItems.length <=1) {
+  if (state.listOfItems.length <= 1) {
     state.listOfItems.splice(0);
   } else {
     state.listOfItems.splice(id, 1);
@@ -55,7 +79,7 @@ const deleteItem = event => {
     state.listOfItems[i].key = i;
     ul.children[i].key = i;
   }
-  state.listOfItems.sort((a,b)=>a.key - b.key);
+  state.listOfItems.sort((a, b) => a.key - b.key);
   localStorage.setItem("0", JSON.stringify(state.listOfItems));
   ul.removeChild(document.querySelector(`[key="${id}"]`));
 };
@@ -95,23 +119,30 @@ const hideDialog = event => {
   dialogAdd.className += " d-none";
   dialogAdd.classList.remove("d-block");
 };
-
+const exitEdit = event => {
+  event.preventDefault();
+  const editDialog = document.querySelector(".edit-dialog");
+  editDialog.className += " d-none";
+  editDialog.classList.remove("d-block");
+};
 //_____hide/show event listener____
 
 clear.addEventListener("click", hideDialog);
+exitBtn.addEventListener("click", exitEdit);
 //___Event listener to show dialog___
 
 buttton.addEventListener(
   "click",
   () => {
+      const error = document.querySelector('.error-d')
+      error.className += ' d-none';
+      error.classList.remove('d-block');
     dialogAdd = document.querySelector(".dialog-add");
     dialogAdd.className += " d-block";
     dialogAdd.classList.remove("d-none");
   },
   false
 );
-
-
 
 const addItem = () => {
   let body = {
@@ -122,13 +153,12 @@ const addItem = () => {
     key: 0,
     checked: false
   };
-console.log('add')
 
-  if (
+  if (nameInp.value.match(/[A-Za-z0-9]/) &&
     nameInp.value.length > 0 &&
     dateTime.value.length > 0
   ) {
-    console.log('add')
+    console.log(nameInp.value.match(/[A-Za-z0-9]/));
     body.name = nameInp.value;
     body.description = descInp.value;
     body.priority = priorityInp.value;
@@ -142,10 +172,97 @@ console.log('add')
     state.listOfItems.push(body);
     console.log(state.listOfItems);
     localStorage.setItem("0", JSON.stringify(state.listOfItems));
-
     ul.innerHTML += renderItem(body);
+    const d = document.querySelector(".dialog-add");
+    d.className += " d-none";
+    d.classList.remove("d-block");
+  } else {
+    const err = document.querySelector(".error-d");
+    err.className += " d-block";
+    err.classList.remove("d-none");
   }
 };
 
+const confirmEdit = event => {
+  const id = event.target.getAttribute("key");
+
+  if (editName.value.match(/[A-Za-z0-9]/)) {
+    state.listOfItems[id].name = editName.value;
+    state.listOfItems[id].description = editDesc.value;
+    state.listOfItems[id].priority = editPriority.value;
+    state.listOfItems[id].date = editDate.value;
+    localStorage.setItem("0", JSON.stringify(state.listOfItems));
+    ul.removeChild(document.querySelector(`p[key="${id}"]`));
+    ul.innerHTML += renderItem(state.listOfItems[id]);
+
+    const d = document.querySelector(".edit-dialog");
+    d.className += " d-none";
+    d.classList.remove("d-block");
+  } else {
+    const err = document.querySelector(".error");
+    err.className += " d-block";
+    err.classList.remove("d-none");
+  }
+};
+
+
+
 //____event listener to set an item______
 addButton.addEventListener("click", addItem, false);
+btnEdit.addEventListener("click", confirmEdit);
+nameInp.addEventListener('keydown', () =>{
+    const error = document.querySelector('.error-d')
+    error.className += ' d-none';
+    error.classList.remove('d-block');
+}, false);
+editName.addEventListener('keydown', () =>{
+    const error = document.querySelector('.error');
+    error.className += ' d-none';
+    error.classList.remove('d-block');
+}, false);
+
+const sortBtn = document.querySelector('#sort');
+
+const sortAllItems = () =>{
+  state.listOfItems.sort((a,b)=>{
+    return +a.priority - (+b.priority)
+  });
+  state.listOfItems.sort((a,b)=>{
+    return Number(a.checked) - Number(b.checked)
+  });
+  console.log(state.listOfItems);
+  for (let i = 0; i < state.listOfItems.length; i++) {
+    state.listOfItems[i].key = i;
+    ul.children[i].key = i;
+  }
+  console.log(state.listOfItems);
+  localStorage.setItem("0", JSON.stringify(state.listOfItems));
+  for (let i = 0; i < state.listOfItems.length; i++){
+    ul.removeChild(document.querySelector(`p[key="${i}"]`))
+  }
+  for (let i = 0; i < state.listOfItems.length; i++){
+    ul.innerHTML += renderItem(state.listOfItems[i]);
+  }
+
+
+};
+
+const searchingItems = (event) =>{
+
+  let inp = event.target.value;
+  let array = state.listOfItems.filter(item => {
+      return item.name.includes(inp)
+  });
+  for (let i = 0; i < array.length; i++){
+    ul.removeChild(document.querySelector(`p[key="${i}"]`))
+  }
+  for (let i = 0; i < array.length; i++){
+    ul.innerHTML += renderItem(array[i]);
+  }
+};
+
+sortBtn.addEventListener('click', sortAllItems);
+
+const search = document.querySelector('#search');
+
+search.addEventListener('keydown', searchingItems, false);
